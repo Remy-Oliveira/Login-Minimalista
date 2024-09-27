097beaa29c54c7e50ada34f83d2e32f7d168af50
Remy-Oliveira <remy.musico@gmail.com>
2024-09-26 22:29:22 -0300

    login-cerrado

---
+++ js.js
@@ -0,0 +1,8 @@
+function ativar() {
+    document.querySelector(".wrapper").classList.add("ativar");
+
+}
+
+function desativar() {
+    document.querySelector(".wrapper").classList.remove("ativar");
+}
